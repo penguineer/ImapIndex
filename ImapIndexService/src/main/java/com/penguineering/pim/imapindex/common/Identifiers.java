3f50c47f65d1e812702bdc51fd3773293b425404
Stefan Haun <mail@tuxathome.de>
2013-02-21 14:09:53 +0100

    Package for common classes and Identifiers interface.

---
+++ ImapIndexService/src/main/java/com/penguineering/pim/imapindex/common/Identifiers.java
@@ -0,0 +1,6 @@
+package com.penguineering.pim.imapindex.common;
+
+public interface Identifiers {
+	public static final String URI_PREFIX = "http://imapindex.penguineering.com";
+
+}
---
+++ ImapIndexService/src/main/java/com/penguineering/pim/imapindex/common/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Stefan Haun (stefan.haun@ovgu.de)
+ *
+ */
+package com.penguineering.pim.imapindex.common;
